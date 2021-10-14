b35ab52b6030c6bee4187518fd90cdb32209ca8c
moud449 <elnajar76@gmail.com>
2021-10-14 17:18:52 +0200

    up

--- archive-job.php
+++ archive-job.php
@@ -8,16 +8,16 @@
       <?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
          <div class="card m-2 " >
           <div class="card-body">
-            <h5 class="card-title">
+            <h5 class="card-title mb-0 pb-0">
               <a href = "<?php the_permalink() ?>">
                 <?php the_title(); ?> 
               </a>
             </h5>
-            <small class="card-subtitle mb-2 text-muted"><?php echo get_field('governorate') ; ?></small>
+            <small class="card-subtitle mb-2 text-muted">📍<?php echo get_field('governorate') ; ?></small>
             <p class="card-text">
-              ـ <?php echo get_field('Market_type'); ?>( <?php echo get_field('job_type'); ?> ) | <?php echo get_field('salary'); ?>
+              🏠<?php echo get_field('Market_type'); ?>( <?php echo get_field('job_type'); ?> ) | 💸<?php echo get_field('salary'); ?>
               <br>
-              ـ <?php echo get_field('owner_name'); ?> . <?php echo get_field('owner_phone'); ?>
+              😌<?php echo get_field('owner_name'); ?> . <?php echo get_field('owner_phone'); ?>
             </p>
           </div>
         </div>
