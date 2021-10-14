066d49a6228261b3e707d8380276e62088e2fa36
moud449 <elnajar76@gmail.com>
2021-10-14 17:23:11 +0200

    up

--- archive-job.php
+++ archive-job.php
@@ -14,10 +14,8 @@
               </a>
             </h5>
             <small class="card-subtitle mb-2 text-muted">📍<?php echo get_field('governorate') ; ?></small>
-            <p class="card-text"> 
-              🏠
-              العمل : 
-              <?php echo get_field('Market_type'); ?>( <?php echo get_field('job_type'); ?> ) | 💸<?php echo get_field('salary'); ?>
+            <p class="card-text" style="direction: ltr"> 
+              🛖<?php echo get_field('Market_type'); ?>( <?php echo get_field('job_type'); ?> ) | 💸<?php echo get_field('salary'); ?>
               <br>
               😌<?php echo get_field('owner_name'); ?> . <?php echo get_field('owner_phone'); ?>
             </p>
