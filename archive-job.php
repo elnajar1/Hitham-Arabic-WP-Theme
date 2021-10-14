e7e7c3ff89726455ba03635f55fc52fc52e5e43a
moud449 <elnajar76@gmail.com>
2021-10-14 16:51:06 +0200

    up

--- archive-job.php
+++ archive-job.php
@@ -7,14 +7,14 @@
     <div class = "col">
       
       <?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
-         <div class="card" style="width: 18rem;">
+         <div class="card m-2 " >
           <div class="card-body">
             <h5 class="card-title">
               <a href = "<?php the_permalink() ?>">
                 <?php the_title(); ?> 
               </a>
             </h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h6 class="card-subtitle mb-2 text-muted"><?php the_field('government'); ?></h6>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
         </div>
