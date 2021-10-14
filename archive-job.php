ef6365b8ee6de377d3e69e97601d9806f069c8ae
moud449 <elnajar76@gmail.com>
2021-10-14 17:04:28 +0200

    up

--- archive-job.php
+++ archive-job.php
@@ -13,9 +13,9 @@
                 <?php the_title(); ?> 
               </a>
             </h5>
-            <small class="card-subtitle mb-2 text-muted"><?php echo get_fields['governorate']; ?></small>
+            <small class="card-subtitle mb-2 text-muted"><?php echo get_fields('governorate') ; ?></small>
             <p class="card-text">
-              <pre><?php var_dump(get_fields()); echo get_field('library'); ?>
+              <pre><?php var_dump(get_fields()); echo get_field('Market_type'); ?>
                . 
               <?php echo get_field('owner_phone'); ?>
             </p>
